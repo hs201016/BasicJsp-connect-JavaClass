b0053fccde4a01c3ae83ab97ba825f18d973c4df
ryuheesu <hs201016@gmail.com>
2023-04-05 14:47:02 +0900

    1st session

--- src/main/java/SpringDatabase/SpringDatabaseApplication.java
+++ src/main/java/SpringDatabase/SpringDatabaseApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDatabaseApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDatabaseApplication.class, args);
 	}
+
+
+	git remote add origin https://github.com/hs201016/BasicJsp-connect-JavaClass.git
+
+
 
 }
